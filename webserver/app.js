27bf4502862dac3303e5da80b0877fe368f4bc9a
Pampapathi K <pampapathi@kalajitha.com>
2024-07-29 11:32:11 +0530

    code updated

--- webserver/app.js
+++ webserver/app.js
@@ -6,11 +6,26 @@ const http = require('http');
 // 1. create server
 // 2. run server
 
+const html = fs.readFileSync('./Template/index.html', 'utf-8') //  here sending the the html content as response when server started
+
 const server = http.createServer((request, response) => {
-    response.end('Hello from the server')
-    console.log('new request running')
+    // response.end('<h1>Hello from the server<h1>')
+    // response.end(html)
+    // console.log('new request running')
     // console.log(request)
     // console.log('pamparesponse', response)
+    let path = request.url;
+    // response.end(path);
+
+    if(path === '/' || path.toLocaleLowerCase() === '/home'){
+        response.end('ure in home page')
+    } else if(path.toLocaleLowerCase() === '/about'){
+        response.end('ure in about page')
+    } else if(path.toLocaleLowerCase() === '/content'){
+        response.end('ure in content page')
+    } else {
+        response.end('Error 404: Page Not found')
+    }
 
 }); // each request will execute the call back function each time new request
 
@@ -31,3 +46,35 @@ server.listen(8000,'127.0.0.1', () =>{
 // domain into actual server address -> match application of server and convert into the server IP host number
 // IP Address + port number + page
 // TCP/IP 
+// Htpp request for the get that we are sedning the get method and resources url and http version
+// and we are sending as request header , url, language, user-agent browser type
+// request body for the request the data from the database to get the data
+// autho
+// HTTP Response -> status code and  response headers ( date, content-type, transfer-encoding) and body the content of the data
+
+
+// 10. how request and response works
+
+// 11. What is Routing
+// ROuting defines the way in which the client request are handled by the application endpoints
+// 1.a File based url
+      // index.html, about.html like that
+// 1.b Resource based url
+        // the routung map name to the physical html file in the application
+
+// Route Paramete
+   // www.nodeapp.com/Product/101
+   //  www.nodeapp.com/Books/Programming/js
+     // here 101 is paramete for getting the specific product details
+
+// Query String
+   // www.nodeapp.com/Books?author=john&id=101
+    // Books?author=john&id=101
+    // key and value sending the request to the api
+    // & symbol for seperate more the 1 query
+
+
+// 12. Creating Routes in NodeJS
+// 13. sending HTML Response instead of text message for the both err and success
+
+
