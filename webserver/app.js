d8432b3220aec87f975e1745b93528bf442def82
Pampapathi K <pampapathi@kalajitha.com>
2024-09-22 13:23:26 +0530

    event driven architecture

--- webserver/app.js
+++ webserver/app.js
@@ -1,89 +1,3 @@
-const readline = require('readline');
-const fs = require('fs');
-const http = require('http');
-const { error, profile } = require('console');
-const path = require('path');
-
-// creating the web server
-// 1. create server
-// 2. run server
-
-const html = fs.readFileSync('./Template/index.html', 'utf-8') //  here sending the the html content as response when server started
-let products = JSON.parse(fs.readFileSync('./data/product.json', 'utf-8'))
-let productListHtml = fs.readFileSync('./Template/product-list.html', 'utf-8');
-let productHtmlArray = products.map((prod)=>{
-        let output = productListHtml.replace('{{%IMAGE}}',prod.productImage);
-      output =  output.replace('{{%NAME%}}', prod.name);
-      output =  output.replace('{{%MODELNAME%}}', prod.modeName);
-      output =   output.replace('{{%MODELNO%}}', prod.modelNumber);
-      output =   output.replace('{{%SIZE%}}', prod.size);
-      output =   output.replace('{{%CAMERA%}}', prod.camera);
-      output =  output.replace('{{%PRICE%}}', prod.price);
-      output =  output.replace('{{%COLOR%}}', prod.color);
-    //   output =  output.replace('{{%ID%}}', prod.id);
-        
-        return output;
-})
-const server = http.createServer((request, response) => {
-    // response.end('<h1>Hello from the server<h1>')
-    // response.end(html)
-    // console.log('new request running')
-    // console.log(request)
-    // console.log('pamparesponse', response)
-    let path = request.url;
-    // response.end(path);
-
-    if (path === '/' || path.toLocaleLowerCase() === '/home') {
-        // response.end(html)
-        response.writeHead(200, {
-            'Content-type': 'text/html',
-            'my-header': 'Hello, World'
-        }) // Setting Headers for response
-        response.end(html.replace('{{%CONTENT%}}', 'You are in Home page'));
-    } else if (path.toLocaleLowerCase() === '/about') {
-        response.writeHead(200, {
-            'Content-type': 'text/html',
-            'my-header': 'Hello, World'
-        })
-        response.end(html.replace('{{%CONTENT%}}', 'You are in about page'))
-    } else if (path.toLocaleLowerCase() === '/contact') {
-        // response.end('ure in content page')
-        response.writeHead(200, {
-            'Content-type': 'text/html',
-            'my-header': 'Hello, World'
-        })
-        response.end(html.replace('{{%CONTENT%}}', 'You are in Contact page'))
-    } else if (path.toLocaleLowerCase() == '/products') {
-        let  productResponseHtml = html.replace('{{%CONTENT%}}', productHtmlArray.join(',')); // assing the html content to here fromthe json array to the html page
-        // response.writeHead(200, {
-        //     'Content-Type': 'application/json'
-        // });
-        response.writeHead(200, {
-            'Content-Type': 'text/html'
-        });
-        response.end(productResponseHtml)
-        console.log(productResponseHtml)
-        // console.log(productHtmlArray.join(','))
-        // fs.readFile('./data/product.json', 'utf-8', (error, data) => {
-        //     let products = json.parse(data)
-        //     response.end(data)
-        // }); // here each time when request call its callling the thie methos instead of this we need call the once time
-
-    }
-    else {
-        // response.end('Error 404: Page Not found')
-        response.writeHead(404)
-        response.end(html.replace('{{%CONTENT%}}', 'Error 404: Page Not found'))
-    }
-
-}); // each request will execute the call back function each time new request
-
-//127.0.0.1:8000
-
-server.listen(8000, '127.0.0.1', () => {
-    console.log('server started');
-})
-
 // 9. how the web works
 
 // client -> 192.168.20.134
@@ -129,5 +43,200 @@ server.listen(8000, '127.0.0.1', () => {
 // 15. working with  JSON Data (1 create folder data, new file produxt.json)
 // convert json data into javascript object use the method Json.parse(data)
 // 16. Transforming JSON data into HTML( create new html product-list.html)
+// 17. Parsing Query string from URL
+// ?id=10&name=phone -> query string
+// 18. creating a reusable function
+// 19. creating a custom module ( a module script export some value, we can use those value to other module) app.js is also a module
+// core modules provided by nodejs, third party modules, user defined module
+// 20. Understanding event-driven architecture
+//  21. Emitting and handling custom events
 
 
+const readline = require('readline');
+const fs = require('fs');
+const http = require('http');
+const { error, profile } = require('console');
+const path = require('path');
+const url = require('url');
+const replaceHtml = require('./Modules/replaceHtml');
+
+// creating the web server
+// 1. create server
+// 2. run server
+
+const html = fs.readFileSync('./Template/index.html', 'utf-8') //  here sending the the html content as response when server started
+let products = JSON.parse(fs.readFileSync('./data/product.json', 'utf-8'))
+let productListHtml = fs.readFileSync('./Template/product-list.html', 'utf-8');
+let productDetailtHtml = fs.readFileSync('./Template/product-details.html', 'utf-8');
+// let productHtmlArray = products.map((prod)=>{
+//         let output = productListHtml.replace('{{%IMAGE}}',prod.productImage);
+//       output =  output.replace('{{%NAME%}}', prod.name);
+//       output =  output.replace('{{%MODELNAME%}}', prod.modeName);
+//       output =   output.replace('{{%MODELNO%}}', prod.modelNumber);
+//       output =   output.replace('{{%SIZE%}}', prod.size);
+//       output =   output.replace('{{%CAMERA%}}', prod.camera);
+//       output =  output.replace('{{%PRICE%}}', prod.price);
+//       output =  output.replace('{{%COLOR%}}', prod.color);
+//       output =  output.replace('{{%ID%}}', prod.id);
+
+//         return output;
+// })
+// function replaceHtml(template, product) {
+//     let output = template.replace('{{%IMAGE}}', product.productImage);
+//     output = output.replace('{{%NAME%}}', product.name);
+//     output = output.replace('{{%MODELNAME%}}', product.modeName);
+//     output = output.replace('{{%MODELNO%}}', product.modelNumber);
+//     output = output.replace('{{%SIZE%}}', product.size);
+//     output = output.replace('{{%CAMERA%}}', product.camera);
+//     output = output.replace('{{%PRICE%}}', product.price);
+//     output = output.replace('{{%COLOR%}}', product.color);
+//     output = output.replace('{{%ID%}}', product.id);
+//     output = output.replace('{{%ROM%}}', product.ROM);
+//     output = output.replace('{{%DESC%}}', product.Description);
+
+//     return output;
+
+// }
+// const server = http.createServer((request, response) => {
+//     // response.end('<h1>Hello from the server<h1>')
+//     // response.end(html)
+//     // console.log('new request running')
+//     // console.log(request)
+//     // console.log('pamparesponse', response)
+//     let { query, pathname: path } = url.parse(request.url, true) // return as object http://localhost:8000/Products?id=10&name=iphone
+//     // console.log(x)
+//     // let path = request.url; // this request url into the main url
+//     // response.end(path);
+
+//     if (path === '/' || path.toLocaleLowerCase() === '/home') {
+//         // response.end(html)
+//         response.writeHead(200, {
+//             'Content-type': 'text/html',
+//             'my-header': 'Hello, World'
+//         }) // Setting Headers for response
+//         response.end(html.replace('{{%CONTENT%}}', 'You are in Home page'));
+//     } else if (path.toLocaleLowerCase() === '/about') {
+//         response.writeHead(200, {
+//             'Content-type': 'text/html',
+//             'my-header': 'Hello, World'
+//         })
+//         response.end(html.replace('{{%CONTENT%}}', 'You are in about page'))
+//     } else if (path.toLocaleLowerCase() === '/contact') {
+//         // response.end('ure in content page')
+//         response.writeHead(200, {
+//             'Content-type': 'text/html',
+//             'my-header': 'Hello, World'
+//         })
+//         response.end(html.replace('{{%CONTENT%}}', 'You are in Contact page'))
+//     } else if (path.toLocaleLowerCase() == '/products') {
+//         if (!query.id) {
+//             let productHtmlArray = products.map((prod) => {
+//                 return replaceHtml(productListHtml, prod)
+//             })
+
+//             let productResponseHtml = html.replace('{{%CONTENT%}}', productHtmlArray.join(',')); // assing the html content to here fromthe json array to the html page
+//             // response.writeHead(200, {
+//             //     'Content-Type': 'application/json'
+//             // });
+//             response.writeHead(200, {
+//                 'Content-Type': 'text/html'
+//             });
+//             response.end(productResponseHtml)
+//         } else {
+//             let prod = products[query.id]
+//             let productDetailResponseHtml = replaceHtml(productDetailtHtml, prod)
+//             response.end(html.replace('{{%CONTENT%}}', productDetailResponseHtml));
+//             console.log(productDetailResponseHtml)
+//         }
+//         // console.log(productHtmlArray.join(','))
+//         // fs.readFile('./data/product.json', 'utf-8', (error, data) => {
+//         //     let products = json.parse(data)
+//         //     response.end(data)
+//         // }); // here each time when request call its callling the thie methos instead of this we need call the once time
+
+//     }
+//     else {
+//         // response.end('Error 404: Page Not found')
+//         response.writeHead(404)
+//         response.end(html.replace('{{%CONTENT%}}', 'Error 404: Page Not found'))
+//     }
+
+// }); // each request will execute the call back function each time new request
+
+//127.0.0.1:8000
+
+const server = http.createServer(); // when ever new request hit the server request listen event
+// event driven architecture
+server.on('request',(request, response) =>{
+    // response.end('<h1>Hello from the server<h1>')
+    // response.end(html)
+    // console.log('new request running')
+    // console.log(request)
+    // console.log('pamparesponse', response)
+    let { query, pathname: path } = url.parse(request.url, true) // return as object http://localhost:8000/Products?id=10&name=iphone
+    // console.log(x)
+    // let path = request.url; // this request url into the main url
+    // response.end(path);
+
+    if (path === '/' || path.toLocaleLowerCase() === '/home') {
+        // response.end(html)
+        response.writeHead(200, {
+            'Content-type': 'text/html',
+            'my-header': 'Hello, World'
+        }) // Setting Headers for response
+        response.end(html.replace('{{%CONTENT%}}', 'You are in Home page'));
+    } else if (path.toLocaleLowerCase() === '/about') {
+        response.writeHead(200, {
+            'Content-type': 'text/html',
+            'my-header': 'Hello, World'
+        })
+        response.end(html.replace('{{%CONTENT%}}', 'You are in about page'))
+    } else if (path.toLocaleLowerCase() === '/contact') {
+        // response.end('ure in content page')
+        response.writeHead(200, {
+            'Content-type': 'text/html',
+            'my-header': 'Hello, World'
+        })
+        response.end(html.replace('{{%CONTENT%}}', 'You are in Contact page'))
+    } else if (path.toLocaleLowerCase() == '/products') {
+        if (!query.id) {
+            let productHtmlArray = products.map((prod) => {
+                return replaceHtml(productListHtml, prod)
+            })
+
+            let productResponseHtml = html.replace('{{%CONTENT%}}', productHtmlArray.join(',')); // assing the html content to here fromthe json array to the html page
+            // response.writeHead(200, {
+            //     'Content-Type': 'application/json'
+            // });
+            response.writeHead(200, {
+                'Content-Type': 'text/html'
+            });
+            response.end(productResponseHtml)
+        } else {
+            let prod = products[query.id]
+            let productDetailResponseHtml = replaceHtml(productDetailtHtml, prod)
+            response.end(html.replace('{{%CONTENT%}}', productDetailResponseHtml));
+            console.log(productDetailResponseHtml)
+        }
+        // console.log(productHtmlArray.join(','))
+        // fs.readFile('./data/product.json', 'utf-8', (error, data) => {
+        //     let products = json.parse(data)
+        //     response.end(data)
+        // }); // here each time when request call its callling the thie methos instead of this we need call the once time
+
+    }
+    else {
+        // response.end('Error 404: Page Not found')
+        response.writeHead(404)
+        response.end(html.replace('{{%CONTENT%}}', 'Error 404: Page Not found'))
+    }
+
+}) // after request then its call the call back function in the call function have a request and response data
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('server started');
+})
+
+// all the event emitter , sever inherits from eventemitter calss
+// observer pattern
+// decoupled module
